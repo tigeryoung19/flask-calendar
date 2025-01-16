55736e34bf18216d785b5468354b456a230976fb
Tiger2loud <348673278@gapps.yrdsb.ca>
2025-01-15 22:12:37 -0500

    add database

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
 
@@ -9,3 +10,26 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+def query_users():
+    try:
+        connection = psycopg2.connect(
+            dbname="neondb",
+            user="neondb_owner",
+            password="QiNF8r1RdzMg",
+            host="ep-green-butterfly-a5cxkofz-pooler.us-east-2.aws.neon.tech",
+            port="5432"
+        )
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return users
+    except Exception as e:
+        return str(e)
+
+@app.route('/users')
+def users():
+    users = query_users()
+    return {'users': users}
